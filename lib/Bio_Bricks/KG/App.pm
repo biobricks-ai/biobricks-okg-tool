9ca9aa346297eeca84e8287b3a7b57f9da591586
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-11-21 17:33:31 -0500

    Quick fzf interface to drill down to primary keys
    From dataset → input file → columns.

--- lib/Bio_Bricks/KG/App.pm
+++ lib/Bio_Bricks/KG/App.pm
@@ -2,11 +2,67 @@ package Bio_Bricks::KG::App;
 # ABSTRACT: App for working with knowledge graphs
 
 use Mu;
+use CLI::Osprey;
 
 use Bio_Bricks::Common::Setup;
+use Bio_Bricks::Common::Types qw( AbsDir );
+
+option base_dir => (
+	required => 0,
+	is       => 'ro',
+	format   => 's',
+	isa      => AbsDir,
+	coerce   => 1,
+	doc      => 'Path to base directory for file paths',
+	default  => sub { Path::Tiny->cwd },
+);
+
+
+use Search::Fzf;
+my %base_fzf_config = (
+	pointer => '>',
+	marker => '*',
+	algo => 'v2',
+);
 
 method run() {
+	use Bio_Bricks::KG::Brick::DataSetFactory;
+	my $datasets = Bio_Bricks::KG::Brick::DataSetFactory->new( base_dir => $self->base_dir )->create;
+	my %dataset_by_name = map { $_->name => $_ } $datasets->@*;
 
+	my $dataset_name = fzf( [ sort keys %dataset_by_name ] , {
+		%base_fzf_config,
+		prompt => 'Choose a dataset > ',
+		multi  => 0,
+	});
+
+	die "Did not select dataset" unless @$dataset_name;
+
+	my %input_by_name = map {
+		$_->input->relative( $self->base_dir ) => $_
+	} $dataset_by_name{$dataset_name->[0]}->inputs->@*;
+
+	my $input_name = fzf( [ sort keys %input_by_name ] , {
+		%base_fzf_config,
+		prompt => 'Choose an input > ',
+		multi  => 0,
+	});
+
+	die "Did not select input" unless @$input_name;
+
+
+	my $input = $input_by_name{$input_name->[0]};
+	my $schema = $input->schema;
+	my %columns_by_name = map { $_->name => $_ } $schema->columns->@*;
+	my $primary_keys = fzf( [ keys %columns_by_name ] , {
+		%base_fzf_config,
+		prompt => 'Choose primary keys > ',
+		sort   => 0,
+		multi  => 1,
+	});
+
+	say '';
+	say join "\n", @$primary_keys;
 }
 
 1;
