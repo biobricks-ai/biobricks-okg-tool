faf7c247f3107439a12d4417c35f82c63c0cdc10
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-12-03 23:49:16 -0500

    Testing ground for YAML processing

--- lib/Bio_Bricks/KG/App/YAML/Processor.pm
+++ lib/Bio_Bricks/KG/App/YAML/Processor.pm
@@ -2,10 +2,35 @@ package Bio_Bricks::KG::App::YAML::Processor;
 # ABSTRACT: Process a YAML schema
 
 use Mu;
-use CLI::Osprey;
+use CLI::Osprey
+	desc => 'Process OKG-ML into RML';
 
-sub run {
-	...
+use Bio_Bricks::Common::Setup;
+use Bio_Bricks::KG::Mapping::OKGML::Model;
+use Bio_Bricks::Common::Types qw( AbsFile );
+
+use YAML qw(LoadFile);
+
+with qw(
+	Bio_Bricks::KG::App::Role::BaseDirOption
+);
+
+option file => (
+	required => 1,
+	is       => 'ro',
+	format   => 's',
+	isa      => AbsFile,
+	coerce   => 1,
+	doc      => 'Path to OKG-ML file',
+);
+
+method run() {
+	my $data = LoadFile( $self->file );
+	my $model = Bio_Bricks::KG::Mapping::OKGML::Model->FROM_HASH(
+		$data
+	);
+
+	use DDP;p $model;
 }
 
 1;
