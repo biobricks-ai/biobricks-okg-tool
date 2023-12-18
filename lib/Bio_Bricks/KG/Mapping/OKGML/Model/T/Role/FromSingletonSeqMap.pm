120f2d5a87ed539b4d536095abd8061874d7043f
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-12-18 10:10:14 -0500

    Improvements to deserialization
    To dump raw data structure back without YAML tags.

--- lib/Bio_Bricks/KG/Mapping/OKGML/Model/T/Role/FromSingletonSeqMap.pm
+++ lib/Bio_Bricks/KG/Mapping/OKGML/Model/T/Role/FromSingletonSeqMap.pm
@@ -3,6 +3,8 @@ package Bio_Bricks::KG::Mapping::OKGML::Model::T::Role::FromSingletonSeqMap;
 
 use Mu::Role;
 use Bio_Bricks::Common::Setup;
+use List::Util qw(pairs);
+use Scalar::Util qw(blessed);
 
 requires 'name';
 
@@ -12,6 +14,28 @@ classmethod FROM_COLLECTION( $data ) {
 		my @keys = keys %$item;
 		die "Multiple mappings inside of sequence" unless @keys == 1;
 		push @obj, $class->new( name => $keys[0], $item->{$keys[0]}->%* );
+	}
+	return \@obj;
+}
+
+method TO_HASH() {
+	my %obj;
+	for my $pair ( pairs %$self ) {
+		my ( $key, $value ) = @$pair;
+		next if $key eq 'name';
+		$obj{$key} =
+			blessed $value && $value->can('TO_HASH')
+				? $value->TO_HASH
+				: $value;
+	}
+	return \%obj;
+}
+
+classmethod TO_COLLECTION( $data ) {
+	my @obj;
+	for my $item ( @$data  ) {
+		my @keys = keys %$item;
+		push @obj, { $item->name => $item->TO_HASH };
 	}
 	return \@obj;
 }
